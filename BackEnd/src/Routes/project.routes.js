eafde71870e1d983ee167eaa8508ee2c6bb70d50
Thiago Goncalves <thiago.goncalves@cepedi.org.br>
2024-10-09 11:47:52 -0300

    Edição do readme e comentario em rotas não utilizadas

--- BackEnd/src/Routes/project.routes.js
+++ BackEnd/src/Routes/project.routes.js
@@ -6,10 +6,10 @@ const teacherController = new TeacherController()
 //Buscar todas as conta
 router.get("/project", async ( request, response ) => { await teacherController.getAllProject( request, response ) })
 //Criar uma conta
-router.post("/project", async (request, response) => { await teacherController.createTeacher( request, response ) })
-//Atualizar um conta
-router.put("/project", async ( request, response) => { await teacherController.updateTeacher( request, response ) })
-//Deletar uma conta pelo id
-router.delete("/project", async ( request, response) => { await teacherController.deleteTeacherId( request, response ) })
+// router.post("/project", async (request, response) => { await teacherController.createTeacher( request, response ) })
+// //Atualizar um conta
+// router.put("/project", async ( request, response) => { await teacherController.updateTeacher( request, response ) })
+// //Deletar uma conta pelo id
+// router.delete("/project", async ( request, response) => { await teacherController.deleteTeacherId( request, response ) })
 
 module.exports = router
--- BackEnd/src/Routes/skill.routes.js
+++ BackEnd/src/Routes/skill.routes.js
@@ -6,10 +6,10 @@ const studentController = new StudentController()
 //Buscar todas as conta
 router.get("/skill", async ( request, response ) => { await studentController.findAllSkill( request, response ) })
 //Criar uma conta
-router.post("/skill", async (request, response) => { await studentController.createStudent( request, response ) })
-//Atualizar um conta
-router.put("/skill", async (request, response) => { await studentController.updateStudent( request, response ) })
-//Deletar uma conta pelo id
-router.delete("/skill", async (request, response) => { await studentController.deleteStudentId( request, response ) })
+// router.post("/skill", async (request, response) => { await studentController.createStudent( request, response ) })
+// //Atualizar um conta
+// router.put("/skill", async (request, response) => { await studentController.updateStudent( request, response ) })
+// //Deletar uma conta pelo id
+// router.delete("/skill", async (request, response) => { await studentController.deleteStudentId( request, response ) })
 
 module.exports = router
